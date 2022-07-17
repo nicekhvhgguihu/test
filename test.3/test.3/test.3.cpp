d90d3b5c7ef3e35406c981899ffeeef30060275f
nicekhvhgguihu <1657295981@qq.com>
2022-07-17 15:31:08 +0800

    熟悉GitHub的使用

---
+++ test.3/test.3/test.3.cpp
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+int main()
+{
+	int x;
+	scanf("%d",&x);
+	printf("hello");
+}
+//严重性	代码	说明	项目	文件	行	禁止显示状态
+//错误	C4996	'scanf': This function or variable may be unsafe.Consider using scanf_s instead.To disable deprecation, use _CRT_SECURE_NO_WARNINGS.See online help for details.test.3	D : \test\test.3\test.3\test.3.cpp	5

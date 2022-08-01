8d3c0a80cecf4c7cdd08e6078b42955bf15126ac
nicekhvhgguihu <1657295981@qq.com>
2022-08-01 11:08:22 +0800

    每日小练

---
+++ test.12/test.12/test.12.cpp
@@ -0,0 +1,39 @@
+﻿//
+////弟弟的作用
+////#include<stdio.h>
+//// int main()
+////{
+////    char op;
+////    int a, b, c, k, score = 0;
+////    while (~(k = scanf("%d%c%d=%d", &a, &op, &b, &c))) {//3 00000000000000000000011 11111111111111000000
+////        if (k == 3) 
+////            getchar();//读取缓存区回车 
+////        else if (op == '+' && c == a + b)
+////            score++;
+////        else if (c == a - b)
+////            score++;
+////    }
+////    printf("%d", score);
+////}
+//
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[32];
+//	
+//	int i = 0;
+//	int k;
+//	while (~(k=scanf("%d", &arr[i])))
+//	{
+//		i++;
+//
+//	}
+//	int sz = i;
+//	int j;
+//	for (j = 0; j < sz; j++)
+//	{
+//		printf("%c",arr[j]);
+//	}
+//}

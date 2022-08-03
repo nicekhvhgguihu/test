2d0caa5645eee68c9b3ba5257f83a6601b3891b9
nicekhvhgguihu <1657295981@qq.com>
2022-08-03 10:27:53 +0800

    每日一练

---
+++ test.13/test.13/test.13.cpp
@@ -0,0 +1,72 @@
+﻿//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	int i;
+//	int x=4;
+//	int f = 1;
+//	scanf("%d",&x);
+//	int arr[1000];
+//	int a=2;
+//	int g = sqrt(x);
+//	for (i = 0; i <= x-2; i++ )
+//	{
+//		arr[i] = a++;
+//	}
+//	while (1)
+//	{
+//		f++;
+//		for (i = 0; i <= x - 2; i++)
+//		{
+//			
+//			if ((arr[i] % f) == 0&&arr[i]!=f)
+//			{
+//				arr[i] = 0;
+//			}
+//		}
+//		if (f % g == 0)
+//		{
+//			break;
+//		}
+//	}
+//	for (i = 0; i <= x - 2; i++)
+//	{
+//		if (arr[i] != 0)
+//		{
+//			printf("%d\n",arr[i]);
+//		}
+//	}
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	while (~(scanf("%d %d", &a, &b)))
+//	{
+//		printf("%d\n",a+b);
+//	}
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	int b, c;
+//	scanf("%d",&a);
+//	int i;
+//	for (i = 0; i < a; i++)
+//	{
+//		scanf("%d %d",&b,&c);
+//		printf("%d\n",b+c);
+//	}
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a=1, b=1;
+//	while (a ||b)
+//	{
+//		scanf("%d %d",&a,&b);
+//		if(a||b)
+//			printf("%d\n",a+b);
+//	}
+//}
